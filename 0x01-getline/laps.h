9e07e2f33415bfd911977e3510f24b3bda47d72c
Abdelkader Raddadi <gadourraddadi@gmail.com>
2022-12-12 19:32:39 +0100

    l

--- 0x01-getline/laps.h
+++ 0x01-getline/laps.h
@@ -1,7 +1,9 @@
 #ifndef lap
 #define lap
+#define MAX = 100
 #include <stdio.h>
-#include <stdlib>
+#include <stdlib.h>
 #include <math.h>
-char *_getline(const int fd);
+
+void race_state(int *id, size_t size);
 #endif
