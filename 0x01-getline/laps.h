83efbb8cff7cec82ca95dc455ab899e48acdd2ad
Abdelkader Raddadi <gadourraddadi@gmail.com>
2022-12-12 17:07:33 +0100

    l

--- 0x01-getline/laps.h
+++ 0x01-getline/laps.h
@@ -3,13 +3,5 @@
 #include <stdio.h>
 #include <stdlib>
 #include <math.h>
-typedef struct reader_s
-{
-	int fd;
-	char *buf;
-	int bytes;
-	struct reader_s *next;
-} reader_t;
-void race_state(int *id, size_t size);
 char *_getline(const int fd);
 #endif
