f116f2e4effddbdfc27b0b75823ac3f683e70339
abdou50 <gadourraddadi@gmail.com>
2023-01-23 11:39:04 +0100

    l

--- 0x03-proc_filesystem/read_write_heap.py
+++ 0x03-proc_filesystem/read_write_heap.py
@@ -1,41 +1,38 @@
 #!/usr/bin/env python3
-'''             
+'''
 Locates and replaces the first occurrence of a string in the heap
-of a process    
+of a process
 
 Usage: ./read_write_heap.py PID search_string replace_by_string
-Where:           
+Where:
 - PID is the pid of the target process
 - search_string is the ASCII string you are looking to overwrite
 - replace_by_string is the ASCII string you want to replace
   search_string with
 '''
 
+
 import sys
 
 
-# check usage  
 if len(sys.argv) != 4:
     print_usage_and_exit()
 
-# get the pid from args
 pid = int(sys.argv[1])
 if pid <= 0:
     print_usage_and_exit()
 search_string = str(sys.argv[2])
-if search_string  == "":
+if search_string == "":
     print_usage_and_exit()
 write_string = str(sys.argv[3])
-if search_string  == "":
+if search_string == "":
     print_usage_and_exit()
 
-# open the maps and mem files of the process
 maps_filename = "/proc/{}/maps".format(pid)
 print("[*] maps: {}".format(maps_filename))
 mem_filename = "/proc/{}/mem".format(pid)
 print("[*] mem: {}".format(mem_filename))
 
-# try opening the maps file
 try:
     maps_file = open('/proc/{}/maps'.format(pid), 'r')
 except IOError as e:
@@ -45,12 +42,10 @@ except IOError as e:
 
 for line in maps_file:
     sline = line.split(' ')
-    # check if we found the heap
     if sline[-1][:-1] != "[heap]":
         continue
     print("[*] Found [heap]:")
 
-    # parse line
     addr = sline[0]
     perm = sline[1]
     offset = sline[2]
@@ -58,15 +53,13 @@ for line in maps_file:
     inode = sline[4]
     pathname = sline[-1][:-1]
 
-    # check if there is read and write permission
     if perm[0] != 'r' or perm[1] != 'w':
         print("[*] {} does not have read/write permission".format(pathname))
         maps_file.close()
         exit(0)
 
-    # get start and end of the heap in the virtual memory
     addr = addr.split("-")
-    if len(addr) != 2: # never trust anyone, not even your OS :)
+    if len(addr) != 2:
         print("[*] Wrong addr format")
         maps_file.close()
         exit(1)
@@ -74,7 +67,6 @@ for line in maps_file:
     addr_end = int(addr[1], 16)
     print("\tAddr start [{:x}] | end [{:x}]".format(addr_start, addr_end))
 
-    # open and read mem
     try:
         mem_file = open(mem_filename, 'rb+')
     except IOError as e:
@@ -83,11 +75,9 @@ for line in maps_file:
         maps_file.close()
         exit(1)
 
-    # read heap  
     mem_file.seek(addr_start)
     heap = mem_file.read(addr_end - addr_start)
 
-    # find string
     try:
         i = heap.index(bytes(search_string, "ASCII"))
     except Exception:
@@ -97,14 +87,11 @@ for line in maps_file:
         exit(0)
     print("[*] Found '{}' at {:x}".format(search_string, i))
 
-    # write the new string
     print("[*] Writing '{}' at {:x}".format(write_string, addr_start + i))
     mem_file.seek(addr_start + i)
     mem_file.write(bytes(write_string, "ASCII"))
 
-    # close files
     maps_file.close()
     mem_file.close()
 
-    # there is only one heap in our example
     break
