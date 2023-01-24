fb4de3f5d549b5999b00504c56f12dc23df24376
abdou50 <gadourraddadi@gmail.com>
2023-01-24 15:24:35 +0100

    l

---
+++ 0x04-readelf/0-hreadelf.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - impliment the readelf
+ * @argc: count of args
+ * @argv: args  passed to program
+ * @env:  the environment
+ * Return: 1 or 0
+ */
+int main(int __attribute__((unused)) argc, char **argv, char **env)
+{
+	char *command[] = {"/usr/bin/readelf", "-W", "-h", "", NULL};
+
+	command[3] = argv[1];
+	if (execve("/usr/bin/readelf", command, env) == -1)
+	{
+		perror("execv");
+		return (0);
+	}
+	return (1);
+}
