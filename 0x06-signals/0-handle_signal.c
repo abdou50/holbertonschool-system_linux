97287dd69a07bb9ad68edf7d311fb01a6bb5e0ba
abdou50 <gadourraddadi@gmail.com>
2023-02-19 15:31:53 +0100

    l

---
+++ 0x06-signals/0-handle_signal.c
@@ -0,0 +1,21 @@
+#include "signals.h"
+/**
+* handler - set a handler
+* @signal: signal
+*/
+void handler(int signal)
+{
+	printf("Gotcha! [%d]\n", signal);
+	fflush(stdout);
+}
+
+/**
+* handle_signal - set a handle
+* Return: 0 on success, -1 in failure
+*/
+int handle_signal(void)
+{
+    if (signal(SIGINT, handler) == SIG_ERR)
+        return (-1);
+    return (0);
+}
---
+++ 0x06-signals/signals.h
@@ -0,0 +1,15 @@
+#ifndef SIGNALS_H
+#define SIGNALS_H
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <signal.h>
+int handle_signal(void);
+void (*current_handler_signal(void))(int);
+int handle_sigaction(void);
+void (*current_handler_sigaction(void))(int);
+int trace_signal_sender(void);
+#endif
