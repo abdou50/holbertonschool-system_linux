27c3fae6a1a3034738a7d180dcaa0681ab77ec0d
abdou50 <gadourraddadi@gmail.com>
2023-02-25 17:58:46 +0100

    l

--- 0x06-signals/4-trace_signal_sender.c
+++ 0x06-signals/4-trace_signal_sender.c
@@ -1,20 +1,30 @@
 #include "signals.h"
-void handler(int sig, siginfo_t *info, void *ucontext)
+/**
+ * handler - function
+ * @numsig: signal number
+ * @info: signal information
+ * @pointer: pointer of the information pid
+ */
+void handler(int numsig, siginfo_t *info, void *pointer)
 {
-    (void) sig;
-    (void) ucontext;
-    if (info)
-    {
-        printf("SIGQUIT send bu %i\n", info->si_pid);
-        fflush(stdout);
-    }
+	(void) numsig;
+	(void) pointer;
+	if (info)
+	{
+		printf("SIGQUIT send bu %i\n", info->si_pid);
+		fflush(stdout);
+	}
 }
+/**
+ * trace_signal_sender - pid of the signal
+ * Return: 1 or 0
+ */
 int trace_signal_sender(void)
 {
-    struct sigaction sa;
+	struct sigaction sa;
 
-    sa.sa_sigaction = handler;
-    sa.sa_flags = SA_SIGINFO;
+	sa.sa_sigaction = handler;
+	sa.sa_flags = SA_SIGINFO;
 
-    return (sigaction(SIGQUIT, &sa, NULL));
+	return (sigaction(SIGQUIT, &sa, NULL));
 }
