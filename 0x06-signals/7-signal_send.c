d599869e021ba57125211867a08f432d7c14cd9e
abdou50 <gadourraddadi@gmail.com>
2023-02-25 15:41:02 +0100

    l

---
+++ 0x06-signals/7-signal_send.c
@@ -0,0 +1,19 @@
+#include "signals.h"
+/**
+ * main - sets a handler for the signal SIGINT
+ * @argc: argument count
+ * @argv: argument value
+ * Return: EXIT_SUCCESS otherwise 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printf("Usage: %s <pid>\n", argv[0]);
+		return (EXIT_FAILURE);
+	}
+	if (kill(atoi(argv[1]), SIGINT) == -1)
+		return (-1);
+	return (1);
+
+}
