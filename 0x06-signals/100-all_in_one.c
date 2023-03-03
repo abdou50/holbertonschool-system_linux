1f9f1401df7d7109b92eadcfda8c49615de2d561
abdou50 <gadourraddadi@gmail.com>
2023-03-04 00:14:44 +0100

    l

---
+++ 0x06-signals/100-all_in_one.c
@@ -0,0 +1,26 @@
+#include "signals.h"
+/**
+ * handler - function
+ * @sig: the signal
+ * @info: info about the sig
+ * @context: the context
+ */
+void handler(int sig, siginfo_t *info, void *context)
+{
+	(void)sig;
+	(void)context;
+	psiginfo(info, "Caught");
+}
+/*
+* all_in_one - that sets up a single handler for all the signals
+*/
+void all_in_one(void)
+{
+	struct sigaction sa;
+	int sig;
+
+	sa.sa_sigaction = handler;
+	sa.sa_flags = SA_SIGINFO;
+	for (sig = 1; sig < SIGRTMAX; sig++)
+		sigaction(sig, &sa, NULL);
+}
