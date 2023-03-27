ee60a70b7624b32c352623923ad77b1a6e372bbe
abdou50 <gadourraddadi@gmail.com>
2023-03-27 20:45:35 +0100

    l

--- 0x08_CPython/4-python.c
+++ 0x08_CPython/4-python.c
@@ -1,27 +1,23 @@
+include <stdio.h>
+#include <stdlib.h>
 #include <Python.h>
 
+/**
+ * print_python_string - prints a Python string using macros
+ * @p: pointer to PyObject
+ */
 void print_python_string(PyObject *p)
 {
-    Py_ssize_t length;
-    wchar_t *wstr;
-    char *str;
-
-    if (!PyUnicode_Check(p)) {
-        fprintf(stderr, "Error: Invalid Python string\n");
-        return;
-    }
-
-    length = PyUnicode_GetLength(p);
-    wstr = PyUnicode_AsWideCharString(p, &length);
-    str = Py_EncodeLocale(wstr, NULL);
-
-    if (str == NULL) {
-        fprintf(stderr, "Error: Unable to convert Python string\n");
-        return;
-    }
-
-    printf("'%s'\n", str);
-
-    PyMem_Free(wstr);
-    PyMem_Free(str);
+	wprintf(L"[.] string object info\n");
+	if (!p || p->ob_type != &PyUnicode_Type)
+	{
+		wprintf(L"  [ERROR] Invalid String Object\n");
+		return (-1);
+	}
+	if (((PyASCIIObject *)p)->state.ascii)
+		wprintf(L"  type: compact ascii\n");
+	else
+		wprintf(L"  type: compact unicode object\n");
+	wprintf(L"  length: %ld\n", ((PyASCIIObject *)p)->length);
+	wprintf(L"  value: %ls\n", PyUnicode_AS_UNICODE(p));
 }
