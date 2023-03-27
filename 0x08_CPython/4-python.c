596496bf99165dcde0a65df077d06ab0806a9169
abdou50 <gadourraddadi@gmail.com>
2023-03-27 20:52:24 +0100

    l*

--- 0x08_CPython/4-python.c
+++ 0x08_CPython/4-python.c
@@ -8,16 +8,16 @@
  */
 void print_python_string(PyObject *p)
 {
-	wprintf(L"[.] string object info\n");
+	wprintf("[.] string object info\n");
 	if (!p || p->ob_type != &PyUnicode_Type)
 	{
-		wprintf(L"  [ERROR] Invalid String Object\n");
+		wprintf("  [ERROR] Invalid String Object\n");
 		return (-1);
 	}
 	if (((PyASCIIObject *)p)->state.ascii)
-		wprintf(L"  type: compact ascii\n");
+		wprintf("  type: compact ascii\n");
 	else
-		wprintf(L"  type: compact unicode object\n");
-	wprintf(L"  length: %ld\n", ((PyASCIIObject *)p)->length);
-	wprintf(L"  value: %ls\n", PyUnicode_AS_UNICODE(p));
+		wprintf("  type: compact unicode object\n");
+	wprintf("  length: %ld\n", ((PyASCIIObject *)p)->length);
+	wprintf("  value: %ls\n", PyUnicode_AS_UNICODE(p));
 }
