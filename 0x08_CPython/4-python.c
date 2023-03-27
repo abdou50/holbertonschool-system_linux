667210735d5b86a62ac5277bdc9ec29eefccf40f
abdou50 <gadourraddadi@gmail.com>
2023-03-27 20:56:03 +0100

    l*

--- 0x08_CPython/4-python.c
+++ 0x08_CPython/4-python.c
@@ -3,21 +3,21 @@
 #include <Python.h>
 
 /**
- * print_python_string - prints a Python string using macros
+ * print_python_string - prints a Python string using macro
  * @p: pointer to PyObject
  */
 void print_python_string(PyObject *p)
 {
-	wprintf("[.] string object info\n");
+	wprintf(L"[.] string object info\n");
 	if (!p || p->ob_type != &PyUnicode_Type)
 	{
-		wprintf("  [ERROR] Invalid String Object\n");
-		return (-1);
+		wprintf(L"  [ERROR] Invalid String Object\n");
+		return;
 	}
 	if (((PyASCIIObject *)p)->state.ascii)
-		wprintf("  type: compact ascii\n");
+		wprintf(L"  type: compact ascii\n");
 	else
-		wprintf("  type: compact unicode object\n");
-	wprintf("  length: %ld\n", ((PyASCIIObject *)p)->length);
-	wprintf("  value: %ls\n", PyUnicode_AS_UNICODE(p));
+		wprintf(L"  type: compact unicode object\n");
+	wprintf(L"  length: %ld\n", ((PyASCIIObject *)p)->length);
+	wprintf(L"  value: %ls\n", PyUnicode_AS_UNICODE(p));
 }
